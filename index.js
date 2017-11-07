97fe12f41cb9d1d1b3f55c2e5e4e2295cb3fc88a
Al Hill <alvarogil91@gmail.com>
2017-11-07 18:15:58 +0100

    First day

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app); //First set of parenthesis call the function, second set of parenthesis are the arguments of the function
 
 const PORT = process.env.PORT || 5000;
 app.listen( PORT );
