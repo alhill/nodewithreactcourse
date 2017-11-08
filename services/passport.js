e498480c6d407db3b91bd03e213b7e2ad875ac33
Al Hill <alvarogil91@gmail.com>
2017-11-08 16:03:25 +0100

    20171108

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,35 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+
+});
+
 passport.use(new GoogleStrategy({
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		}, 
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
-		}
-	)
-);
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	},
+	(accessToken, refreshToken, profile, done) => {
+		User.findOne({
+				googleId: profile.id
+			})
+			.then((existingUser) => {
+				if (existingUser) {
+					done(null, existingUser); //first argument informs of an error
+					//if we have a record with this ID
+				} else {
+					//if not, make a new record
+					new User({
+							googleId: profile.id
+						})
+						.save()
+						.then(user => done(null, user));
+				}
+			})
+	}
+));
