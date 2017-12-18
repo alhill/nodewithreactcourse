e11d10c9b47cb7adb564d354acdf6dd44d7332cd
Al Hill <alvarogil91@gmail.com>
2017-12-18 12:23:07 +0100

    20171218

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div>
+			<h1>
+				Formulario
+			</h1>
+			Formulario para recoger feedback de los cursos
+		</div>
+	);
+};
+
+export default Landing;
