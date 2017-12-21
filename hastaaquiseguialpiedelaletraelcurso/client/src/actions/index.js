e498ac706190ac2e3c27e9ba5abd1d3419123ea7
Al Hill <alvarogil91@gmail.com>
2017-12-21 13:30:40 +0100

    20171221 - 2

--- hastaaquiseguialpiedelaletraelcurso/client/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
-
-export const fetchUser = () => async (dispatch) => { //Pasamos 'dispatch' como argumento de la función de retorno
-	const res = await axios.get('/api/current_user')
-	dispatch({ type: FETCH_USER, payload: res.data }); //Usamos dispatch cuando la promesa que crea axios.get se ha completado
-}
