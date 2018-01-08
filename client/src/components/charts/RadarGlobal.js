2fb9809e99846114c58bba90a6e86b75c2e31f8d
Al Hill <alvarogil91@gmail.com>
2018-01-08 18:03:35 +0100

    Prueba de radar doble

--- client/src/components/charts/RadarGlobal.js
+++ client/src/components/charts/RadarGlobal.js
@@ -12,10 +12,11 @@ class RadarGlobal extends Component {
 	componentWillReceiveProps( props ){
 		
 		const graphArr = [];
+		const industry = [8,7,6,9,5,6,4];
 		props.surveys.map( (survey, i) => {	
 			const ratingavg = Array.from(survey.answers, item => item.rating).reduce( (a, b) => (a + b), 0 )/(survey.answers.length || 1);
 			const name = survey.title ;		
-			graphArr.push({ name: name, rating: ratingavg });
+			graphArr.push({ name: name, rating: ratingavg, industryrating: industry[i] });
 		});
 		this.setState({ graphArr });
 	}
@@ -25,9 +26,10 @@ class RadarGlobal extends Component {
 		console.log( this.state.graphArr );
 		return(
 			<RadarChart cx={300} cy={250} outerRadius={100} width={600} height={500} data={this.state.graphArr}>
-          <Radar name="Mike" dataKey="rating" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
+         	<Radar name="Media del sector" dataKey="industryrating" stroke="#773333" fill="#bb5555" fillOpacity={0.6}/>
+          <Radar name="Tu empresa" dataKey="rating" stroke="#4544bb" fill="#8884d8" fillOpacity={0.6}/>
           <PolarGrid />
-				{/*<Legend />*/}
+				  <Legend />
           <PolarAngleAxis dataKey="name" />
           <PolarRadiusAxis angle={30} domain={[0, 10]}/>
       </RadarChart>
